2eb43775ff4303fad0f5a282d66c8f418a1233b2
Mihail Nazarov <msnazarow@gmail.com>
2021-10-02 05:23:30 +0300

    Initial Commit

---
+++ PersonalDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalDiary
+//
+//  Created by out-nazarov2-ms on 02.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
